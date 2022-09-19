ea6750b49c1bbc39c179fe40f97d16cc94f9253d
unknown <jesusgmontiel23@outlook.com>
2022-09-19 10:14:37 -0500

    Cambio de equipo

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $user = "root";
-    $clave = "Hacienda.123";
+    $clave = "";
     $bd = "sis_inventario";
     $conexion = mysqli_connect($host,$user,$clave,$bd);
     if (mysqli_connect_errno()){
